a200375bcaeece26f81281eaf14cdaa6ae2d0c80
Alessio Moraschini <info@am-design-development.com>
2024-06-22 12:21:34 +0200

    improved gitignore, data file renamed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO SABE_USER (id, name, surname, age) VALUES (1, 'Alessio', 'Moraschini', 33);
+INSERT INTO SABE_USER (id, name, surname, age) VALUES (2, 'Nano', 'Moraschini', 6);
+INSERT INTO SABE_USER (id, name, surname, age) VALUES (3, 'Fefe', 'Moraschini', 5);
+INSERT INTO SABE_USER (id, name, surname, age) VALUES (4, 'Armando', 'Maradona', 98);
+INSERT INTO SABE_USER (id, name, surname, age) VALUES (5, 'Hail', 'ToTheKing', 100);
