4cc575743847199a7b5e7c0ab5f67c75e5bacecf
Alessio Moraschini <info@am-design-development.com>
2025-02-24 05:02:04 +0100

    Cleaned unused stuff

--- src/main/java/com/am/design/development/utilities/facade/RemoteRetrieverFacade.java
+++
@@ -1,9 +0,0 @@
-package com.am.design.development.utilities.facade;
-
-import java.io.File;
-import java.io.IOException;
-
-public interface RemoteRetrieverFacade {
-
-    File downloadVideo(String videoUrl) throws IOException;
-}
