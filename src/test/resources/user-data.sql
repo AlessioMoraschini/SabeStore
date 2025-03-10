95435bf31b99a5fafb31b87c7a1d40dc0c1019dc
Alessio Moraschini <info@am-design-development.com>
2025-03-10 03:51:00 +0100

    AM-7 fix in tests for H2 inserts

--- src/test/resources/user-data.sql
+++ src/test/resources/user-data.sql
@@ -1,3 +1,6 @@
+INSERT INTO USER_ROLE (id, role) VALUES (1, 'USER');
+INSERT INTO USER_ROLE (id, role) VALUES (2, 'SUPERUSER');
+
 INSERT INTO SABE_USER (id, name, surname, mail, role_id, password, age) VALUES (1, 'Alessio', 'Moraschini', 'alessio.moraschini@hotmail.it', 2, '$2a$10$vm8hJMZHV1q7RkGFxkNojeV9ILm3w1auHzMoNVtZqXTcQtg9Cibc2', 33);
 INSERT INTO SABE_USER (id, name, surname, mail, role_id, password, age) VALUES (2, 'Nano', 'Moraschini', 'a.b@mail.it', 1, '$2a$10$A7.32vMAcLq81lEwaGEhAuCdUDWtfacvb4L3eHFNmz2lPWJIeA3v.', 6);
 INSERT INTO SABE_USER (id, name, surname, mail, role_id, password, age) VALUES (3, 'Fefe', 'Moraschini', 'b.b@mail.it', 1, '$2a$10$A7.32vMAcLq81lEwaGEhAuCdUDWtfacvb4L3eHFNmz2lPWJIeA3v.', 5);
@@ -5,5 +8,3 @@ INSERT INTO SABE_USER (id, name, surname, mail, role_id, password, age) VALUES (
 INSERT INTO SABE_USER (id, name, surname, mail, role_id, password, age) VALUES (5, 'Hail', 'ToTheKing', 'd.b@mail.it', 1, '$2a$10$A7.32vMAcLq81lEwaGEhAuCdUDWtfacvb4L3eHFNmz2lPWJIeA3v.', 100);
 
 
-INSERT INTO USER_ROLE (id, role) VALUES (1, 'USER');
-INSERT INTO USER_ROLE (id, role) VALUES (2, 'SUPERUSER');
